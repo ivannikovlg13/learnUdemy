fb345d9721bac86307576eadf62d2ebff9f5289d
Alexander <livannikov55@gmail.com>
2020-06-20 14:31:33 +0300

    lesson 13 if,switch

--- baza/js/script.js
+++ baza/js/script.js
@@ -1,49 +1,38 @@
-// 'use strict';
-// //1
-// let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-// //2
-// let personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {
-//         question1: lastWatchFilm,
-//         question2: rateFilm
-//     },
-//     actors: {},
-//     genres: [],
-//     privat: false
+'use strict';
 
-// };
-
-// //3
-
-
-// var lastWatchFilm = prompt('Один из последних просмотренных фильмов ? ');
-// var rateFilm = prompt('На сколько оцените его? ');
+if(1){
+  console.log('OK!');
+} else{
+  console.log('Error');
+}
 
 
 
 
-//решение
+// if(num < 49){
+//   console.log('Error');
+// } else if(num > 100 ){
+//   console.log('Many');
+// } else{
+//   console.log('Ok!');
+// }
 
-//1 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//2
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors: {},
-    genres:[],
-    privat:false
+// (num === 50) ? console.log('OK!'): console.log('Error');
 
-};
+const num = '50';
 
-const a = prompt('Один из последних просмотренных фильмов ?', ''),
-      b = prompt('На сколько оцените его?' , ''),
-      c = prompt('Один из последних просмотренных фильмов ?', ''),
-      d = prompt('На сколько оцените его?', '');
+switch (num) {
+  case '49':
+    console.log('Не верно');
+    break;
+  case '100':
+    console.log('Не верно');
+    break;
+  case '50':
+    console.log('В точку');
+  break;
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+  default:
+    console.log('Не в этот раз');
+    break;
+}
