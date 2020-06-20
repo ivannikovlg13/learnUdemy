bd3378898902d22771af4ebf875f7022e1e4243e
Alexander <livannikov55@gmail.com>
2020-06-20 10:27:36 +0300

    first commit

---
+++ baza/js/lesson7.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+const answers =[];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your lastsame?', '');
+// answers[2] = prompt('How old are you?', '');
+
+
+console.log(typeof(answers));
+console.log(typeof(null));
---
+++ baza/js/lesson8.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
+
+//DZ
+
+let name2 = 'Lena';
+
+console.log(`Your name is ${name2}`);
