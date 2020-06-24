083d3f4cefd43d7325a8e797de86bc3187f0ed99
Alexander <livannikov55@gmail.com>
2020-06-24 13:23:12 +0300

    added callBack,objects,strings files

---
+++ baza/js/callBack.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function first(){
+  //do something
+  setTimeout(function (){
+    console.log(1);
+  }, 500);
+}
+
+function second(){
+  console.log(2);
+}
+
+first();
+second();
+
+
+function learnJs(lang, callback) {
+  console.log(`Я учу: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('Я прошел этот урок');
+}
+
+learnJs('Я учу JavaScript', done);
